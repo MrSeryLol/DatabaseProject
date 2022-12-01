41adf153af650c36e6642f2cfc5aa29293004e88
bib4maru <kirillosanov@yandex.ru>
2022-12-01 20:20:36 +0300

    Исправление конфликтов

--- DbManager/OfficeEquipmentContext.cs
+++ DbManager/OfficeEquipmentContext.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-<<<<<<< HEAD
-=======
 using System.Windows;
->>>>>>> remotes/origin/master
 using DbManager.Entities;
 using Microsoft.EntityFrameworkCore;
 
