0b15335b96d418946038bdb9517dfc5c5cb7e480
MrSeryLol <psygentelmen014@gmail.com>
2022-12-03 00:20:03 +0300

    New Commit!

--- OfficeEquipment/MainWindow.xaml.cs
+++ OfficeEquipment/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using DbManager.Entities;
 using DbManager.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
